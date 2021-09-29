d8761e6725d14770a186a64e22f95d89bc5b2bc8
Suhani Zaman <suhanizaman15@gmail.com>
2021-09-29 23:51:15 +0400

    refactored spec file

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,48 +2,43 @@ require './lib/bank.rb'
 require 'spec_helper'
 
 describe Bank do
-    it " default  balance of the customer" do
+    subject(:bank) {Bank.new}
+    it "customer bank balance, checks if there's an account created" do
         expect(subject.balance).to eq 0
     end
-    it "on deposting balance is increased" do
-        bank = Bank.new 
-        balance = bank.deposit(1000,"10/01/2023")
+
+    it "customer deposits money to the account on the specified date" do
+        bank.deposit(1000,"10/01/2023")
         expect(bank.balance).to eq 1000
     end
-    it "Display the current bank statement after deposting " do
-        bank = Bank.new 
-        balance = bank.deposit(1000,"10/01/2023")
+    it "Display the current bank statement after crediting money " do
+        bank.deposit(1000,"10/01/2023")
         expect{bank.display}.to output('date || credit || debit || balance
 10/01/2023 || 1000.0 || || 1000.0').to_stdout
     end
-    it "depositing balance is increased" do
-        bank = Bank.new
+    it "customer credits money to the existing account on specified date " do
         bank.balance = 1000
-        balance = bank.deposit(2000,"13/01/2023")
+        bank.deposit(2000,"13/01/2023")
         expect(bank.balance).to eq 3000
     end
-    it "Display the current bank statement after deposting" do
-        bank = Bank.new
+    it "Display the current bank statement after money credited to account with balance amount of 1000 " do
         bank.balance = 1000
-        balance = bank.deposit(2000,"13/01/2023")
+        bank.deposit(2000,"13/01/2023")
         expect{bank.display}.to output('date || credit || debit || balance
 13/01/2023 || 2000.0 || || 3000.0').to_stdout
     end
-    it "on withdrawing balance is decreased" do
-        bank = Bank.new 
+    it "customer withraws money from the bank on the specified date" do
         bank.balance = 3000
-        balance = bank.withdraw(500, "14/01/2023")
+        bank.withdraw(500, "14/01/2023")
         expect(bank.balance).to eq 2500
     end
-    it "Display the current bank statement after withdrawing" do
-        bank = Bank.new
+    it "Display the current bank statement after money withdrawn" do
         bank.balance = 3000
-        balance = bank.withdraw(500, "14/01/2023")
+        bank.withdraw(500, "14/01/2023")
         expect{bank.display}.to output('date || credit || debit || balance
 14/01/2023 || || 500.0 || 2500.0').to_stdout
     end
-    it "Display the  bank statement after withdrawing" do
-        bank = Bank.new
+    it "Displays the full bank trasaction made by the customer" do
         bank.deposit(1000,"10/01/2023")
         bank.deposit(2000,"13/01/2023")
         bank.withdraw(500, "14/01/2023")
